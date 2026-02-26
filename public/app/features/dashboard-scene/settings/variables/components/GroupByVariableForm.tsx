7cea8765e0d8f85d1fae0866f135c1851de900c6
Kristina Demeshchik <kristina.demeshchik@gmail.com>
2026-02-26 12:48:32 -0500

    testid

--- public/app/features/dashboard-scene/settings/variables/components/GroupByVariableForm.tsx
+++ public/app/features/dashboard-scene/settings/variables/components/GroupByVariableForm.tsx
@@ -83,12 +83,7 @@ export function GroupByVariableForm({
       ) : null}
 
       {datasourceSupported && onDefaultValueChange && (
-        <DefaultValueEditor
-          values={defaultValue ?? []}
-          options={defaultValueOptions}
-          onChange={onDefaultValueChange}
-          data-testid={selectors.pages.Dashboard.Settings.Variables.Edit.GroupByVariable.defaultValueSection}
-        />
+        <DefaultValueEditor values={defaultValue ?? []} options={defaultValueOptions} onChange={onDefaultValueChange} />
       )}
 
       {datasourceSupported && (
