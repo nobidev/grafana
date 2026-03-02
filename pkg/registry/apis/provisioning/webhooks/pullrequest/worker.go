c7b9d28c8192baf8f73558c7557ec7a412099ad1
Daniele Ferru <daniele.ferru@grafana.com>
2026-03-02 09:45:09 +0100

    stop blocking folder creation in parser

--- pkg/registry/apis/provisioning/webhooks/pullrequest/worker.go
+++ pkg/registry/apis/provisioning/webhooks/pullrequest/worker.go
@@ -15,6 +15,7 @@ import (
 	"github.com/grafana/grafana/pkg/registry/apis/provisioning/resources"
 	"github.com/grafana/grafana/pkg/registry/apis/provisioning/utils"
 	"github.com/grafana/grafana/pkg/services/apiserver"
+	"github.com/grafana/grafana/pkg/services/featuremgmt"
 	"github.com/grafana/grafana/pkg/services/rendering"
 	"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/storage/unified/resource"
@@ -23,6 +24,7 @@ import (
 
 func ProvidePullRequestWorker(
 	cfg *setting.Cfg,
+	features featuremgmt.FeatureToggles,
 	renderer rendering.Service,
 	blobstore resource.ResourceClient,
 	configProvider apiserver.RestConfigProvider,
@@ -35,7 +37,7 @@ func ProvidePullRequestWorker(
 	// FIXME: we should create providers for client and parsers, so that we don't have
 	// multiple connections for webhooks
 	clients := resources.NewClientFactory(configProvider)
-	parsers := resources.NewParserFactory(clients)
+	parsers := resources.NewParserFactory(clients, features.IsEnabledGlobally(featuremgmt.FlagProvisioningFolderMetadata)) //nolint:staticcheck
 	screenshotRenderer := NewScreenshotRenderer(renderer, blobstore)
 	evaluator := NewEvaluator(screenshotRenderer, parsers, urlProvider, registry)
 	commenter := NewCommenter(cfg.ProvisioningAllowImageRendering)
