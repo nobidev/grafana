3ee956a48cd47842fcdb970181b603b5b54b2997
Jack Westbrook <jack.westbrook@gmail.com>
2026-04-02 10:30:17 +0200

    chore(frontend-service): renamed boot script feature flag and update error msg

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -1775,5 +1775,5 @@ export interface FeatureToggles {
   * Boots the frontend using the boot.js script built from TS instead of the embedded boot script
   * @default false
   */
-  inlinedBootScript?: boolean;
+  compiledBootScript?: boolean;
 }
