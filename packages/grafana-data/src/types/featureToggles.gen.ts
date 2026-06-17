d7cbb76fe8e03d2c1f378af24e0cab7bb546bdbe
grambbledook <denis.vodopianov@grafana.com>
2026-06-17 16:41:04 +0200

    initial commit

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -489,11 +489,6 @@ export interface FeatureToggles {
   */
   sqlExpressionsCodeMirror?: boolean;
   /**
-  * Enable grafana's embedded kube-aggregator
-  * @default false
-  */
-  kubernetesAggregator?: boolean;
-  /**
   * Enable CAP token based authentication in grafana's embedded kube-aggregator
   * @default false
   */
--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -951,15 +951,6 @@ var (
 			Expression:  "false",
 		},
 		{
-			Name:            "kubernetesAggregator",
-			Description:     "Enable grafana's embedded kube-aggregator",
-			Stage:           FeatureStageExperimental,
-			Owner:           grafanaAppPlatformSquad,
-			RequiresRestart: true,
-			Expression:      "false",
-			Generate:        Generate{LegacyGo: true, LegacyFrontend: true},
-		},
-		{
 			Name:            "kubernetesAggregatorCapTokenAuth",
 			Description:     "Enable CAP token based authentication in grafana's embedded kube-aggregator",
 			Stage:           FeatureStageExperimental,
@@ -1293,7 +1284,6 @@ var (
 			Expression:  "true", // Enabled by default for now
 			Generate:    Generate{LegacyGo: true, LegacyFrontend: true},
 		},
-
 		{
 			Name:         "vizActionsAuth",
 			Description:  "Allows authenticated API calls in actions",
