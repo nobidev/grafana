3c7f8b7f5ed9c91664ecabd59744d23e8263fe1a
Alejandro Malavet <alejandro.malavet@grafana.com>
2026-07-09 11:29:32 -0500

    Provisioning: inline files commit author lookup

--- pkg/registry/apis/provisioning/files.go
+++ pkg/registry/apis/provisioning/files.go
@@ -19,6 +19,7 @@ import (
 	"github.com/grafana/grafana/apps/provisioning/pkg/repository/git"
 	"github.com/grafana/grafana/apps/provisioning/pkg/safepath"
 	"github.com/grafana/grafana/pkg/apimachinery/apis/common/v0alpha1"
+	"github.com/grafana/grafana/pkg/apimachinery/identity"
 	"github.com/grafana/grafana/pkg/apimachinery/utils"
 	"github.com/grafana/grafana/pkg/registry/apis/provisioning/resources"
 )
@@ -89,8 +90,8 @@ func (c *filesConnector) Connect(ctx context.Context, name string, opts runtime.
 // handleRequest processes the HTTP request for files operations.
 func (c *filesConnector) handleRequest(ctx context.Context, name string, r *http.Request, responder rest.Responder, logger logging.Logger) {
 	if userAttributionEnabled(ctx) {
-		if sig, ok := auth.GetAuthorFromRequester(ctx); ok {
-			ctx = repository.WithAuthorSignature(ctx, sig)
+		if id, err := identity.GetRequester(ctx); err == nil && id.IsIdentityType(authlib.TypeUser) {
+			ctx = repository.WithAuthorSignature(ctx, repository.CommitSignature{Name: id.GetName(), Email: id.GetEmail()})
 		}
 	}
 	repo, err := c.getRepo(ctx, r.Method, name)
