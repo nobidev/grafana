b764ade574de44f6b5a58e3bebf36a4eb1bbdcd4
Matt Jacobson <matthew.jacobson@grafana.com>
2026-08-11 23:50:43 -0400

    Alerting: Move ManagedRoute and helper functions to v1 package
    Moves `ManagedRoute`, `ManagedRoutes`, and `CalculateRouteFingerprint` from `legacy_storage` to `v1` package.

--- pkg/services/ngalert/notifier/legacy_storage/imported.go
+++ pkg/services/ngalert/notifier/legacy_storage/imported.go
@@ -113,7 +113,7 @@ func (e ImportedConfigRevision) ReceiverUseByName() map[string]int {
 	return m
 }
 
-func (e ImportedConfigRevision) GetManagedRoute() (*ManagedRoute, error) {
+func (e ImportedConfigRevision) GetManagedRoute() (*v1.ManagedRoute, error) {
 	if e.importedConfig == nil {
 		return nil, nil
 	}
@@ -124,7 +124,7 @@ func (e ImportedConfigRevision) GetManagedRoute() (*ManagedRoute, error) {
 
 	merge.RenameResourceUsagesInRoutes([]*v1.Route{route}, renamed)
 
-	mr := NewManagedRoute(e.identifier, route)
+	mr := v1.NewManagedRoute(e.identifier, route)
 	mr.Provenance = models.ProvenanceConvertedPrometheus
 	mr.Origin = models.ResourceOriginImported
 	return mr, nil
